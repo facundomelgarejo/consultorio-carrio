5d3e056431dee0e1992f2ac623df6b71a828c588
Facundo Melgarejo <64665231+facundomelgarejo@users.noreply.github.com>
2022-07-05 12:33:50 -0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import Header from '../../Commons/Header/Header';
+import React, {useState} from "react";
+import Header from "../../Commons/Header/Header";
+import Button from "../../Commons/Button/button";
 import '../Home/Home.css';
 
-const Home = () =>{
+
+const Home = () => {
     return(
+    <div>
         <Header />
+        <div className="conteiner-buttons">
+            <div className="conteiner-buttons-up">
+            <button className="buttons">Buscar Paciente</button>
+            </div>
+
+            <div className="conteiner-buttons-down">
+            <button className="buttons">Lista Pacientes</button>
+            <button className="buttons">Agenda</button>
+            </div>
+        </div>
+    </div>
     )
 }
 
